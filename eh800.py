94a08d0ce3cef2f967a859d799b2420ab27bf87b
Jarno Lehtinen <lehtinen@sci.fi>
2022-06-01 23:24:39 +0300

    Update eh800.py

--- eh800.py
+++ eh800.py
@@ -2,18 +2,21 @@
 
 # sudo apt install python3-pip python3-serial
 # (sudo apt install python3-matplotlib)
+# (sudo apt install python3-paho-mqtt)
 # sudo pip3 install xmodem
 
 PORT = '/dev/ttyACM0'
 FILENAME = 'trend.log'
 
-import sys, time, serial
-from xmodem import XMODEM
-import matplotlib.pyplot as plt
-#import numpy as np
+INTERVAL=300
 
-#ser = serial.Serial(PORT, timeout=0) # or whatever port you need
-ser = serial.Serial(PORT, timeout=2)
+#VERBOSE = True
+VERBOSE = False
+
+import sys, time, datetime, serial
+from xmodem import XMODEM
+#import matplotlib.pyplot as plt
+import paho.mqtt.client as mqtt
 
 def getc(size, timeout=1):
   return ser.read(size) or None
@@ -22,13 +25,13 @@ def putc(data, timeout=1):
   return ser.write(data)  # note that this ignores the timeout
 
 def send_cmd(cmd):
-  ser.write(bytes(cmd, 'ASCII') + b'\n')
-  print('cmd: ' + cmd)
+  ser.write(bytes(cmd, 'ASCII') + b'\r\n')
+  print('*cmd: ' + cmd)
 
 def receive_line(verbose = True):
   line = ser.readline().decode("ASCII").rstrip("\r\n")
   if verbose:
-    print('reply: ' + line) # b'Start receiving now.\r\n'
+    print('*reply: ' + line) # b'Start receiving now.\r\n'
   return line
 
 def download(filename):
@@ -37,18 +40,18 @@ def download(filename):
   send_cmd(cmd) # b'upload trend.log\n'
   receive_line() # b'Start receiving now.\r\n'
   stream = open(FILENAME, 'wb')
-  print('LOG: Start receiving (XMODEM-CRC)')
+  print('*Start receiving (XMODEM-CRC)')
   ser.flushInput()
   modem.recv(stream, crc_mode=1)
   line = receive_line() # b'28822 Bytes Transmitted.\r\n'
   size = int(line.split()[0])
   stream.truncate(size)
   stream.close()
-  print('LOG: downloaded ' + filename + ' (' + str(size) + ' bytes)')
+  print('*downloaded ' + filename + ' (' + str(size) + ' bytes)')
   return size
 
 def read_measurement(v):
-  line = receive_line(False)
+  line = receive_line(VERBOSE)
   if line != '':
     value = float(line.split()[0])
     v.append(value)
@@ -61,20 +64,13 @@ def get_measurements(verbose = False):
     read_measurement(values)
 
   if verbose:
-    print(values)
-  print('Menovesi: ' + str(values[8]))
-  print('Ulkoilma: ' + str(values[9]))
-  print('Venttiili: ' + str(values[14]))
+    print('*', values)
+  v = [ values[8], values[9], values[14] ]
+  print('*Menovesi: ' + str(v[0]))
+  print('*Ulkoilma: ' + str(v[1]))
+  print('*Venttiili: ' + str(v[2]))
+  return v
 
-
-#send_cmd('time') # b'time\n'
-#receive_line() # b'Wednesday, 2020-10-21 18:53.58\r\n'
-
-if len(sys.argv) > 1:
-  if str(sys.argv[1]) == 'download':
-    download(FILENAME)
-
-#get_measurements()
 
 #00000000: 9d00 a005 1400 0200 0300 0400 0500 0900  ................
 #00000010: 0800 1001 3201 1737 8f5f 0200 8c01 f1d8  ....2..7._......
@@ -85,64 +81,188 @@ if len(sys.argv) > 1:
 #00000060: 0000 0400 0000 2830 8f5f 0000 7a01 f1d8  ......(0._..z...
 #00000070: f1d8 f1d8 0000 0900 0000 8032 8f5f 0000  ...........2._..
 #00000080: 8201 f1d8 f1d8 f1d8 0000 0700 0000 d834  ...............4
+def graph():
+  file = open(FILENAME, 'rb')
+  #"header" 22 bytes
+  #one entry 20 bytes
+  file.seek(0, 2) # end of file
+  filesize = file.tell()
+  file.seek(0, 0) # start of file
+  samples = int((filesize - 22) / 20)
+  print(samples)
+  header = file.read(22)
+  values = []
+  x1 = []
+  y1 = []
+  for c in range(samples):
+    data = file.read(20)
+    epoch = (data[3] << 24) + (data[2] << 16) + (data[1] << 8) + data[0]
+    ulko = ((data[5] << 8) + data[4]) / 10.0
+    meno = ((data[7] << 8) + data[6]) / 10.0
+    vent = ((data[17] << 8) + data[16]) / 1.0
+    values.append([ epoch, ulko, meno, vent ])
+    y1.append(float(meno))
 
-file = open(FILENAME, 'rb')
-#"header" 22 bytes
-#one entry 20 bytes
-file.seek(0, 2) # end of file
-filesize = file.tell()
-file.seek(0, 0) # start of file
-samples = int((filesize - 22) / 20)
-print(samples)
-header = file.read(22)
-values = []
-x1 = []
-y1 = []
-for c in range(samples):
-  data = file.read(20)
-  epoch = (data[3] << 24) + (data[2] << 16) + (data[1] << 8) + data[0]
-  ulko = ((data[5] << 8) + data[4]) / 10.0
-  meno = ((data[7] << 8) + data[6]) / 10.0
-  vent = ((data[17] << 8) + data[16]) / 1.0
-  values.append([ epoch, ulko, meno, vent ])
-  y1.append(float(meno))
+  #print(epoch)
+  date = time.strftime('%d.%m.%Y %H:%M:%S', time.gmtime(epoch))
+  #print(values)
+  file.close()
 
-#print(epoch)
-date = time.strftime('%d.%m.%Y %H:%M:%S', time.gmtime(epoch))
-#print(date)
-#print(ulko)
-#print(meno)
-#print(vent)
-#print(values)
-file.close()
+  for x in range(samples):
+    epoch = values[x][0]
+    x1.append(time.strftime('%H:%M', time.gmtime(epoch)))
 
-for x in range(samples):
-#for x in range(100):
-#  x1.append(x)
-  epoch = values[x][0]
-  x1.append(time.strftime('%H:%M', time.gmtime(epoch)))
+  total = 160
+  plt.plot(x1[0:total], y1[0:total])
+  plt.xlabel('Aika')
+  plt.ylabel('°C')
+  plt.title('Menovesi')
+  x_ticks = x1[:total:20]
+  plt.xticks(x_ticks)
+  #plt.axhline(y=5,color='gray')
+  plt.grid()
 
-#print(x1)
-#plt.plot(x1, y1)
-total = 160
-plt.plot(x1[0:total], y1[0:total])
-plt.xlabel('Aika')
-plt.ylabel('°C')
-plt.title('Menovesi')
+  plt.show()
 
-#fig, ax = plt.subplots()
-#width = np.diff(x1).min()
-#ax.bar(x1, y1, align='center', width=width)
-#ax.xaxis_date()
-#fig.autofmt_xdate()
+def on_connect(client, userdata, flags, rc):
+  print("*Connected with result code "+str(rc))
+  #client.subscribe("$SYS/#")
+  global flag_connected
+  if rc == 0:
+    print("*Connection OK")
+    flag_connected = 1
+  else:
+    print("*Connection error!")
+    flag_connected = 0
 
-#x_ticks = [0, 16, 5]
-x_ticks = x1[:total:20]
-plt.xticks(x_ticks)
+def on_publish(client, userdata, rc): #create function for callback
+  #print("data published \n")
+  print("*Data published with result code "+str(rc))
+  #pass
 
-#plt.axhline(y=5,color='gray')
-plt.grid()
+def sendMQTTmsg():
+  print('*' + str(datetime.datetime.now()))
+  msg = '{ "v0":' + str(v[0]) + ', "v1":' + str(v[1]) + ', "v2":' + str(v[2]) + ' }'
+  print("*MQTT-message: " + msg)
+  client1.publish("ouman", msg)
 
-plt.show()
+#--------------------------------
+# MAIN
+#--------------------------------
+print("McGurk Ouman MQTT-utility")
+print(" Usage parameters: download [filename] or once")
+print(" Without any parameters goes to MQTT-loopmode")
 
+ser = serial.Serial(PORT, timeout=2)
+
+send_cmd('time') # b'time\n'
+receive_line() # b'Wednesday, 2020-10-21 18:53.58\r\n'
+
+ONCE = False
+if len(sys.argv) > 1:
+  if str(sys.argv[1]) == 'download':
+    download(FILENAME)
+  if str(sys.argv[1]) == 'once':
+    ONCE = True
+
+f = open(sys.path[0]+"/credentials.txt", "r")
+exec(f.read())
+f.close()
+#print(MQTT_SERVER)
+#print(MQTT_PORT)
+#print(MQTT_USERNAME)
+#print(MQTT_PASSWORD)
+#print(MQTT_API_KEY)
+
+flag_connected = 0
+
+#--- ONCE ---
+
+if ONCE:
+  v = get_measurements(VERBOSE)
+  #print(v)
+  ser.close()
+  client1 = mqtt.Client("", clean_session=True)
+  client1.on_connect = on_connect # callback
+  client1.on_publish = on_publish # callback
+  client1.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
+  client1.connect(MQTT_SERVER, MQTT_PORT) # establish connection
+  timeout = 5
+  timeout_start = time.time()
+  while not flag_connected:
+    client1.loop()
+    if time.time() > timeout_start + timeout:
+      print("*Connect error!")
+      sys.exit()
+  sendMQTTmsg()
+  client1.disconnect()
+  sys.exit()
+
+#--- LOOP ---
+
+client1 = mqtt.Client("", clean_session=True)
+client1.on_connect = on_connect # callback
+client1.on_publish = on_publish # callback
+client1.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
+lwm = '{ "v0":"NaN", "v1":"NaN", "v2":"NaN" }'
+client1.will_set("ouman", lwm)
+while not flag_connected:
+  try:
+    print("*Trying to connect")
+    client1.connect(MQTT_SERVER, MQTT_PORT, keepalive=INTERVAL*2+10) # establish connection
+  except Exception as e:
+    print("*client1.connect() failed")
+    print(e)
+    time.sleep(5)
+    continue
+  #time.sleep(2)
+  client1.loop_start()
+  #time.sleep(2)
+  timeout = 5
+  timeout_start = time.time()
+  while not flag_connected:
+    client1.loop()
+    if time.time() > timeout_start + timeout:
+      print("*Connection timeout error!")
+      break
+  #    sys.exit()
+  #client1.loop()
+  #time.sleep(2)
+print("*Connected")
+#timeout = 5
+#timeout_start = time.time()
+#while not flag_connected:
+#  client1.loop()
+#  if time.time() > timeout_start + timeout:
+#    print("*Connection timeout error!")
+#    sys.exit()
+
+next_reading = time.time()
+#client1.loop_start()
+
+try:
+  while True:
+    v = get_measurements(VERBOSE)
+    #print(v)
+    #msg = '{ "v0":' + str(v[0]) + ', "v1":' + str(v[1]) + ', "v2":' + str(v[2]) + ' }'
+    #print("*MQTT-message: " + msg)
+    #client1.publish("ouman", msg)   #publish
+    sendMQTTmsg()
+    next_reading += INTERVAL
+    sleep_time = next_reading-time.time()
+    if sleep_time > 0:
+      time.sleep(sleep_time)
+except KeyboardInterrupt:
+  pass
+
+client1.loop_stop()
+v[0] = "NaN"
+v[1] = "NaN"
+v[2] = "NaN"
+#v[0] = float("NaN")
+#v[1] = float("NaN")
+#v[2] = float("NaN")
+sendMQTTmsg()
+client1.disconnect()
 ser.close()
+
